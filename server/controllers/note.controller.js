913c6d651899fe7e2a69a459535871f849ec3c71
Paweł Konik <konius85@gmail.com>
2018-01-18 17:49:38 +0100

    Add updateTask updateLane

--- server/controllers/note.controller.js
+++ server/controllers/note.controller.js
@@ -1,4 +1,64 @@
 import Note from '../models/note';
+import Lane from '../models/lane';
+import uuid from 'uuid';
+
+
+export function addNote(req, res) {
+  const { note, laneId } = req.body;
+// console.log(note, laneId, req.body);
+  if (!note || !note.task || !laneId) {
+    res.status(400).end();
+  }
+
+  const newNote = new Note({
+    task: note.task,
+  });
+
+  newNote.id = uuid();
+  newNote.save((err, saved) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    Lane.findOne({ id: laneId })
+      .then(lane => {
+        lane.notes.push(saved);
+        return lane.save();
+      })
+      .then(() => {
+        res.json(saved);
+      });
+  });
+}
+
+export function updateNote(req, res) {
+
+  if (!req.params.noteId) {
+    res.status(403).end();
+  }
+
+  Note.findOneAndUpdate({ id: req.params.noteId},{task: req.body.task}).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    note.save(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+export function deleteNote(req, res) {
+  Note.findOne({ id: req.params.noteId }).exec((err, note) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    note.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
 
 export function getSomething(req, res) {
   return res.status(200).end();
--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,9 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
-
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+router.route('/lanes').get(LaneController.getLanes);
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+router.route('/lanes').put(LaneController.updateLane);
 export default router;
